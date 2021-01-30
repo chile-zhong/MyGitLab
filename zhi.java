166c718b44caf9a6e1b54fb05984472c9fec02f3
chile zhong <470707540@qq.com>
2021-01-30 19:48:20 +0800

    Update zhi.java

--- zhi.java
+++ zhi.java
@@ -1,1 +1,8 @@
-hello world
+
+介绍无障碍
+介绍华为与谷歌的区别(在wiki上找资料)
+介绍用途 手机中的无障碍app 举例 搜狗输入法  应用宝
+https://juejin.cn/post/6844903456809943053
+https://ljd1996.github.io/2020/01/14/Android-AccessibilityService/
+https://blog.csdn.net/qq_38499859/article/details/90523283 Android之辅助服务上篇————AccessibilityService使用
+https://blog.csdn.net/qq_38499859/article/details/93770643 Android之辅助服务上篇————AccessibilityService使用
