36e6189e6d68ca1ff9d77851738e3894e5a22f9d
chile zhong <470707540@qq.com>
2022-11-10 22:17:45 +0800

    Update zhi.java

--- zhi.java
+++ zhi.java
@@ -1,25 +1,106 @@
+List map排序
+List:
+数值型:
+1.Collections.reverse(intList); 原序列的倒叙
+2.intList.sort(Comparator.naturalOrder()); 正序
+intList.sort(Comparator.reverseOrder()) 倒序
+字符型: 
+strList.sort(Collator.getInstance(Locale.CHINA)); 正序
+strList.sort(Collator.getInstance(Locale.CHINA).reversed()); 倒序
+JavaBean类型:
 
-介绍无障碍
-介绍华为与谷歌的区别(在wiki上找资料)
-介绍用途 手机中的无障碍app 举例 搜狗输入法  应用宝  抢红包  点击广告
-遇到过的问题以及大家曾经遇到的问题也可以分享一下
-https://juejin.cn/post/6844903456809943053  AccessibilityService 从入门到出轨
-https://ljd1996.github.io/2020/01/14/Android-AccessibilityService/
-https://blog.csdn.net/qq_38499859/article/details/90523283 Android之辅助服务上篇————AccessibilityService使用
-https://blog.csdn.net/qq_38499859/article/details/93770643 Android之辅助服务下篇————AccessibilityService使用
-https://developer.android.com/guide/topics/ui/accessibility/service
-https://cloud.tencent.com/developer/article/1485280  Android：AccessibilityService辅助功能基础使用(附微信抢红包教程)
-http://lionoggo.com/2018/03/22/%E6%B7%B1%E5%85%A5Android%E8%BE%85%E5%8A%A9%E6%9C%8D%E5%8A%A1%E6%9E%B6%E6%9E%84%E4%B8%8E%E8%AE%BE%E8%AE%A1/  深入Android辅助服务架构与设计
-https://blog.csdn.net/hj7jay/article/details/54860773 抢红包代码
-  
-调试disble  为什么使用位操作  是如何保存到xml中的  android.view.accessibility.AccessibilityNodeInfo#init是在什么时候调用的  试一下去掉disable也能实现同样的功能
-android.view.accessibility.AccessibilityNodeInfo#writeToParcel什么时候调用
-~旖旎中子 12:10:28
-百度  20190514000297491    iRWXxqcfTJNR8u9qKd8k
+Hashmap的排序:
+1.利用List和Collections来实现排序	
 
-~旖旎中子 12:10:45
-搜狗    affac42d87f55298dcd51d67e0a505cf    53f3de8cbc7eb8b78aefbcdb3ba18089
+2.map.entrySet().stream().sorted(Map.Entry.<String, Double>comparingByValue()).forEach(System.out::println);
+二分
+public int search(int[] nums, int target) {
+    int lo = 0;
+    int hi = nums.length - 1;
+    while (lo <= hi) {
+        int mid = lo - (lo - hi) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) lo = mid + 1;
+        else if (nums[mid] > target) hi = mid - 1;
+    }
+    return -1;
+}
 
-~旖旎中子 12:11:00
-有道    6bbf039ce2f38c08   Fg0bCSnWK8htErrHtU6Xx641JAyFlmBb
+滑动窗口
+public String slideWindow(String s, String t) {
+    // 实时记录窗口内的数据 (只存放 need 中需要的字符)
+    Map<Character, Integer> window = new HashMap<>();
+    // 记录目标字符，始终保持不变
+    Map<Character, Integer> need = new HashMap<>();
+    // 初始化 need 目标窗口
+    // code...
+    
+    // 左闭右开 [left, right)
+    int left = 0, right = 0;
+    
+    // 存放满足收缩条件的变量
+    // valid
+    
+    while (right < s.length()) {
+        // 1. 窗口扩张
+        char c = s.charAt(right);
+        right++;
+        // 2. 更新 valid，window 数据
+        if (need.containsKey(c)) {
+            // 更新 window
+            // code...
+            
+            // 更新 valid
+            if (条件) {
+                // code...
+            }
+        }
+        // 根据 valid，来判断是否需要收缩
+        while (valid == need.size()) {
+            // 1. 判断结果是否需要更新
+            // code...
+            
+            // 2. 窗口收缩
+            char d = s.charAt(left);
+            left++;
+            // 3. 更新 valid，window 数据
+            if (need.containsKey(d)) {
+                // 更新 valid
+                if (条件) {
+                    // code...
+                }
+                
+                // 后更新 window
+                // code...
+            }
+        }
+    }
+    return res;
+}
 
+快排
+
+归并
+DFS
+BFS
+public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) return res;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.offer(root);
+    while (!q.isEmpty()) {
+        int size = q.size();
+        List<Integer> list = new ArrayList<>();
+        // 完整的一个循环即表示一层
+        for (int i = 0; i < size; i++) {
+            TreeNode cur = q.poll();
+            list.add(cur.val);
+            if (cur.left != null) q.offer(cur.left);
+            if (cur.right != null) q.offer(cur.right);
+        }
+        res.add(list);
+    }
+    return res;
+}
+
+位运算
